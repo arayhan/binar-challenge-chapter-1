640f1059058bae5f5aac475a65a66ef99f8afb85
Rayhan <rayhanprima99@gmail.com>
2022-08-26 20:27:45 +0700

    chore: change separator and add soal 4

--- challenge-02/index.js
+++ challenge-02/index.js
@@ -1,6 +1,7 @@
-// ========================
-// SOAL 1
-// ========================
+console.log("\n===========================");
+console.log("SOAL 1");
+console.log("===========================");
+
 const changeWord = (text, selectedText, changedText) => {
     return text.replace(selectedText, changedText);
 };
@@ -11,9 +12,10 @@ const kalimat2 = "Gunung bromo tak akan mampu menggambarkan besarnya cintaku pad
 console.log(changeWord(kalimat1, "mencintai", "membenci"));
 console.log(changeWord(kalimat2, "bromo", "semeru"));
 
-// ========================
-// SOAL 2
-// ========================
+console.log("\n===========================");
+console.log("SOAL 2");
+console.log("===========================");
+
 const checkTypeNumber = (givenNumber) => {
     if (!givenNumber) return "Error: Bro where is the parameter?";
     else if (typeof givenNumber !== "number") return "Error: Invalid data type";
@@ -28,9 +30,9 @@ console.log(checkTypeNumber({}));
 console.log(checkTypeNumber([]));
 console.log(checkTypeNumber());
 
-// ========================
-// SOAL 3
-// ========================
+console.log("\n===========================");
+console.log("SOAL 3");
+console.log("===========================");
 
 const checkEmail = (email) => {
     const EMAIL_PATTERN =
@@ -38,7 +40,7 @@ const checkEmail = (email) => {
 
     if (!email) return "Error: Bro where is the parameter?";
 
-    return String(email).match(EMAIL_PATTERN) ? "VALID" : "INVALID";
+    return EMAIL_PATTERN.test(email) ? "VALID" : "INVALID";
 };
 
 console.log(checkEmail("apranata@binar.co.id"));
@@ -47,3 +49,37 @@ console.log(checkEmail("apranata@binar"));
 console.log(checkEmail("apranata")); // ERROR: karena tidak sesuai dengan email pattern
 console.log(checkTypeNumber(checkEmail(3322))); // ERROR: Invalid & return ke check type number berupa error string
 console.log(checkEmail()); // ERROR: Bro where is the parameter?
+
+console.log("\n===========================");
+console.log("SOAL 4");
+console.log("===========================");
+
+const isValidPassword = (givenPassword) => {
+    const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (givenPassword === undefined) return "Error: Bro where is the parameter?";
+    else if (typeof givenPassword !== "string") return "Error: Invalid data type";
+    return PASSWORD_PATTERN.test(givenPassword);
+};
+
+console.log(isValidPassword("Meong2021"));
+console.log(isValidPassword("meong2021"));
+console.log(isValidPassword("@eong"));
+console.log(isValidPassword("Meong2"));
+console.log(isValidPassword(0)); // ERROR: karena bukan string, invalid data type
+console.log(isValidPassword()); // ERROR: undefined
+
+console.log("\n===========================");
+console.log("SOAL 5");
+console.log("===========================");
+
+console.log("\n===========================");
+console.log("SOAL 6");
+console.log("===========================");
+
+console.log("\n===========================");
+console.log("SOAL 7");
+console.log("===========================");
+
+console.log("\n===========================");
+console.log("SOAL 8");
+console.log("===========================");
