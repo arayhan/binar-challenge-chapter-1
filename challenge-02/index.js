b613af50de5add2196c18b138b748ce18492b7f9
Rayhan <rayhanprima99@gmail.com>
2022-08-26 21:57:16 +0700

    chore (Challege 2): add soal 7 & 8

--- challenge-02/index.js
+++ challenge-02/index.js
@@ -111,6 +111,114 @@ console.log("\n===========================");
 console.log("SOAL 7");
 console.log("===========================");
 
+const dataPenjualanPakAldi = [
+    {
+        namaProduct: "Sepatu Futsal Nike Vapor Academy 8",
+        hargaSatuan: 760000,
+        kategori: "Sepatu Sport",
+        totalTerjual: 90,
+    },
+    {
+        namaProduct: "Sepatu Warrior Tristan Black Brown High",
+        hargaSatuan: 960000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 37,
+    },
+    {
+        namaProduct: "Sepatu Warrior Tristan Maroon High",
+        hargaSatuan: 360000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 90,
+    },
+    {
+        namaProduct: "Sepatu Warrior Rainbow Tosca Corduroy",
+        hargaSatuan: 120000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 90,
+    },
+];
+
+const getTotalPenjualan = (dataPenjualan) => {
+    if (dataPenjualan === undefined) return "ERROR: Bro where is the parameter?";
+    else if (typeof dataPenjualan !== "object") return "ERROR: Invalid data type";
+
+    return dataPenjualan.reduce((acc, penjualan) => acc + penjualan.totalTerjual, 0);
+};
+
+console.log(getTotalPenjualan(dataPenjualanPakAldi));
+
 console.log("\n===========================");
 console.log("SOAL 8");
 console.log("===========================");
+
+const dataPenjualanNovel = [
+    {
+        idProduct: "BOOK002421",
+        namaProduk: "Pulang - Pergi",
+        penulis: "Tere Liye",
+        hargaBeli: 60000,
+        hargaJual: 86000,
+        totalTerjual: 150,
+        sisaStok: 17,
+    },
+    {
+        idProduct: "BOOK002351",
+        namaProduk: "Selamat Tinggal",
+        penulis: "Tere Liye",
+        hargaBeli: 75000,
+        hargaJual: 103000,
+        totalTerjual: 171,
+        sisaStok: 20,
+    },
+    {
+        idProduct: "BOOK002941",
+        namaProduk: "Garis Waktu",
+        penulis: "Fiersa Besari",
+        hargaBeli: 67000,
+        hargaJual: 99000,
+        totalTerjual: 213,
+        sisaStok: 5,
+    },
+    {
+        idProduct: "BOOK002941",
+        namaProduk: "Laskar Pelangi",
+        penulis: "Andrea Hirata",
+        hargaBeli: 55000,
+        hargaJual: 68000,
+        totalTerjual: 20,
+        sisaStok: 56,
+    },
+];
+
+const formatRupiah = (number) => {
+    const result = number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&.");
+    return `Rp ${result}`;
+};
+
+const calculatePercentage = (number) => {
+    return String(number);
+};
+
+const getInfoPenjualan = (dataPenjualan) => {
+    if (dataPenjualan === undefined) return "ERROR: Bro where is the parameter?";
+    else if (typeof dataPenjualan !== "object") return "ERROR: Invalid data type";
+
+    const totalKeuntungan = dataPenjualan.reduce((acc, penjualan) => acc + penjualan.hargaJual * penjualan.totalTerjual, 0);
+    const totalModal = dataPenjualan.reduce((acc, penjualan) => acc + penjualan.hargaBeli * (penjualan.totalTerjual + penjualan.sisaStok), 0);
+    const persentaseKeuntungan = (totalKeuntungan / totalModal) * 100;
+    const produkTerlaris = dataPenjualan.sort((a, b) => {
+        aTerlaris = a.totalTerjual / (a.totalTerjual + a.sisaStok);
+        bTerlaris = b.totalTerjual / (b.totalTerjual + b.sisaStok);
+        return aTerlaris - bTerlaris;
+    });
+
+    return {
+        totalKeuntungan: formatRupiah(totalKeuntungan),
+        totalModal: formatRupiah(totalModal),
+        persentaseKeuntungan: `${String(persentaseKeuntungan).substr(0, 6)}%`,
+        produkBukuTerlaris: produkTerlaris[0].namaProduk,
+        penulisTerlaris: produkTerlaris[0].penulis,
+    };
+};
+
+console.log(getInfoPenjualan(dataPenjualanNovel));
